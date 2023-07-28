a87c8776fc45fe105264383b1aee2da847ab7670
pedro-imbriani-clear <pedro.imbriani.clear@gmail.com>
2023-07-28 00:33:26 -0300

    Slider

--- App.js
+++ App.js
@@ -1,61 +1,40 @@
-import React, { Component } from 'react';
-import { View, StyleSheet, Text} from 'react-native';
-
-import {Picker} from '@react-native-picker/picker';
-
-export default class App extends Component {
+import React, { Component } from 'react'
+import { View, Text, StyleSheet} from 'react-native'
+import Slider from '@react-native-community/slider'
+class App extends Component {
 
   constructor(props){
-    super(props);
+    super(props)
     this.state = {
-      pizza: 0,
-      pizzas: [
-        {key: 1, nome: 'Strogonoff', valor: 35.90},
-        {key: 2, nome: 'Calabresa', valor: 59},
-        {key: 3, nome: 'Quatro queijos', valor: 37},
-        {key: 4, nome: 'Brigadeiro', valor: 25.70},
-        {key: 5, nome: 'Portuguesa', valor: 70},
-      ]
-    };
-  };
+      valor: 0
+    }
+  }
 
- render(){
+  render(){
 
-  let pizzasItem = this.state.pizzas.map( (v, k) => {
-    return <Picker.Item key={k} value={k} label={v.nome} />
-  } )
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.logo}>Menu Pizza</Text>
-
-      <Picker
-      selectedValue={this.state.pizza}
-      onValueChange={ (itemValue, itemIndex) =>  this.setState({pizza: itemValue}) }
-      >
-       {pizzasItem}
-      </Picker>
-
-      <Text style={styles.pizzas}>Voce escolheu: {this.state.pizzas[this.state.pizza].nome}</Text>
-      <Text style={styles.pizzas}>R$: {this.state.pizzas[this.state.pizza].valor.toFixed(2)}</Text>
-    </View>
-   );
- }
+    return(
+      <View style={styles.container}>
+        <Slider 
+          inimumValue={0}
+          maximumValue={100} 
+          onValueChange={ (valorSelecionado) => this.setState({valor: valorSelecionado}) }
+          value={this.state.valor}
+          minimumTrackTintColor={'green'}
+          maximumTrackTintColor={'red'}
+        />
+        <Text>{this.state.valor.toFixed()}</Text>
+        
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container:{
-    flex:1,
-    marginTop: 20,
+    flex: 1,
+    marginTop: 20
   },
-  logo:{
-    textAlign: 'center',
-    fontSize: 28,
-    fontWeight: 'bold'
-  },
-  pizzas:{
-    marginTop: 15,
-    fontSize: 25,
-    textAlign: 'center'
-  }
-});
+
+})
+
+export default App;
